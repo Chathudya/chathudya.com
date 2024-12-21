d03bf67e4290819ea67c999b0674b16f5758f178
Chathudya Jayawardana <chathudyajayawardana@Chathudyas-MacBook-Air.local>
2024-12-21 13:51:27 +0530

    Updates

--- precache-manifest.e136b13f01ab4d6bb824498494adbda6.js
+++ precache-manifest.e136b13f01ab4d6bb824498494adbda6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc3d4a3d6fd09ab64ae327b1210df6de",
+    "revision": "3f27b8a829779cbbb6d5ca7284518a42",
     "url": "/chathudya.com/index.html"
   },
   {
-    "revision": "02b5011b6cc08ad30ff2",
+    "revision": "c577496b59849e6c7378",
     "url": "/chathudya.com/static/css/main.f4b342b7.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/chathudya.com/static/js/3.132715ea.chunk.js"
   },
   {
-    "revision": "02b5011b6cc08ad30ff2",
-    "url": "/chathudya.com/static/js/main.01a8266b.chunk.js"
+    "revision": "c577496b59849e6c7378",
+    "url": "/chathudya.com/static/js/main.aa435255.chunk.js"
   },
   {
     "revision": "922a0ec890b0061dd43f",
