614566df57dde60f13921354451a781772ade6d5
Chathudya Jayawardana <chathudyajayawardana@Chathudyas-MacBook-Air.local>
2024-12-24 19:18:21 +0530

    Update About section with style improvements

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,11 +7,7 @@ function Home() {
             <h2>Welcome to My Portfolio</h2>
             <p>Your one-stop solution to learn more about me!</p>
             <div className="profile-image-container">
-                <img
-                    src="/images/profile.jpeg"
-                    alt="Chathudya"
-                    className="profile-image"
-                />
+                <img src={`${process.env.PUBLIC_URL}/images/profile.jpeg`} alt="Chathudya" className="profile-image"/>
             </div>
         </div>
     );
